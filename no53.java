ba9e1ebc6278a0b1ad09847a5c867eae6595172e
Oluwatobiloba Ogunbi <tobiogunbi@gmail.com>
2022-02-26 15:47:41 -0500

    lol

---
+++ no53.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        ArrayList<Integer> lis = new ArrayList<>();
+        int largestsum = nums[0];
+        for(int i=0;i<nums.length;i++){
+            lis.add(index, element);
+        }
+    }
+}
